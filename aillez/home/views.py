20c40386a64e3dbe195d84daf5ed8dd64a41437a
Vitor Hugo da Silva Lima <silvalimavitorhugo@gmail.com>
2020-08-01 11:16:01 -0500

    Adding git ignore file

--- aillez/home/views.py
+++ aillez/home/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "home/index.html")
+
+
+def sobre(request):
+    return render(request, "sobre/sobre.html")
