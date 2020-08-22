e30fbb74a3224bb675a7e67e7adcb10fee3f2602
Vitor Hugo da Silva Lima <silvalimavitorhugo@gmail.com>
2020-08-22 11:45:30 -0500

    Changing settings

--- aillez/aillez/settings.py
+++ aillez/aillez/settings.py
@@ -119,6 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/potious/app/static/'
+STATIC_ROOT = '/var/www/aillez/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/var/www/potious/app/media/'
+MEDIA_ROOT = '/var/www/aillez/media/'
