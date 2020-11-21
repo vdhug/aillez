462e3b01fb84be14e6c50bfcdf75295d69e70662
Vitor Hugo da Silva Lima <silvalimavitorhugo@gmail.com>
2020-11-20 21:43:24 -0500

    Adicionando pagina de estudo

--- aillez/blog/urls.py
+++ aillez/blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="estudos-de-caso"),
+    path("<slug:slug>/", views.estudo, name="estudo"),
 ]
--- aillez/blog/views.py
+++ aillez/blog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "blog/estudos.html")
+
+def estudo(request, slug):
+    return render(request, "blog/estudo.html")
